9861a979b9b238c29de381883b3eb1860ad7c812
yanhgx <mohammadd.arrdiansyahh@gmail.com>
2024-01-24 21:23:13 +0700

    add javascript for create

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import dataTodo from './data/dataTodo'
 
 function App() {
   const [todos, setTodos] = useState(dataTodo)
+  const onCreate = (newTodo) => {
+    setTodos([...todos,{id:Math.round(Math.random()*99999), ...newTodo}])
+  }
   return (
     <div className='container py-5'>
       <h1 className='fw-normal fs-2'>TodoList App</h1>
       <p>Add your daily activity</p>
-      <TodoForm/>
+      <TodoForm onCreate={onCreate}/>
       <TodoList todos={todos}/>
     </div>
   );
--- src/component/TodoForm.js
+++ src/component/TodoForm.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({onCreate}) => {
+  const initialState = {
+    todo:''
+  }
+  const [formTodo, setFormTodo] = useState(initialState)
+  const {todo} = formTodo
+  const handleChangeTodo = (e) => {
+    setFormTodo({...formTodo, [e.target.name]: e.target.value})
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(formTodo)
+    setFormTodo(initialState)
+  }
   return (
     <div>
-        <form className='d-flex gap-2'>
-            <input type='text' name='todo' id='todo' className='px-3' placeholder='Add your task...' autoComplete='off'/>
-            <input type='submit' name='add' id='add' value='Add' className='btn btn-primary rounded-0 px-4'/>
-        </form>
+      <form className='d-flex gap-2' onSubmit={handleSubmit}>
+        <input type='text' name='todo' id='todo' className='px-3' placeholder='Add your task...' 
+        autoComplete='off' onChange={handleChangeTodo} value={todo}/>
+        <input type='submit' name='add' id='add' value='Add' className='btn btn-primary rounded-0 px-4'/>
+      </form>
     </div>
   )
 }
